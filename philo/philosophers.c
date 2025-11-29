43b62c33cfcbb008618a3c57e432f5a1b1d57d9f
Davi Santos <davisimoesdossantos1@gmail.com>
2025-11-29 15:39:46 -0300

    fix: reestructured the way philosophers die

--- philo/philosophers.c
+++ philo/philosophers.c
@@ -6,24 +6,17 @@
 /*   By: dasimoes <dasimoes@42sp.org.br>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/17 15:59:32 by dasimoes          #+#    #+#             */
-/*   Updated: 2025/11/25 13:14:01 by dasimoes         ###   ########.fr       */
+/*   Updated: 2025/11/25 15:43:35 by dasimoes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void	*act_philo(void *ptr)
+int	act_decide(t_philo *philo)
 {
 	struct timeval		now;
-	t_philo				*philo;
 	int					time;
 
-	philo = (t_philo *) ptr;
-	if (!philo->lifetime.tv_sec)
-	{
-		if (gettimeofday(&(philo->lifetime), NULL) == -1)
-			philo->control->error = "gettimeofday error";
-	}
 	if (philo->action == TAKING_FORK)
 		taking_fork(philo);
 	else if (philo->action == SLEEPING)
@@ -34,19 +27,40 @@ void	*act_philo(void *ptr)
 		philo->control->error = "gettimeofday error";
 	time = (now.tv_sec / 1000) - (philo->lifetime.tv_sec / 1000); 
 	if (time >= philo->control->time_to_die)
+	{
 		philo->action = DYING;
+		return (0);
+	}
+	return (1);
+}
+
+void	*act_philo(void *ptr)
+{
+	t_philo				*philo;
+
+	philo = (t_philo *) ptr;
+	if (!philo->lifetime.tv_sec)
+	{
+		if (gettimeofday(&(philo->lifetime), NULL) == -1)
+			philo->control->error = "gettimeofday error";
+	}
+	while (act_decide(philo))
+		;
 	return (NULL);
 }
 
 void	*init_checker(void *control)
 {
-	t_control	*con;
-	t_philo		*cur;
+	pthread_mutex_t		mutex;
+	t_control			*con;
+	t_philo				*cur;
 
 	con = (t_control *) control;
 	cur = con->head;
+	pthread_mutex_init(&mutex, NULL);
 	while (1)
 	{
+		pthread_mutex_lock(&mutex);
 		if (cur->action == DYING || con->eating_times == 0)
 		{
 			con->check = 1;
@@ -54,10 +68,12 @@ void	*init_checker(void *control)
 				notify(cur, con, DYING);
 			break ;
 		}
+		cur = cur->next;
+		pthread_mutex_unlock(&mutex);
 		if (cur->next == con->head)
 			usleep(5000);
-		cur = cur->next;
 	}
+	pthread_mutex_destroy(&mutex);
 	return (NULL);
 }
 
@@ -68,21 +84,18 @@ void	init_simulation(t_control *con)
 	cur = con->head;
 	if (pthread_create(&(con->checker), NULL, init_checker, (void *)con))
 		con->error = "pthread_create error";
-	while (!con->check)
+	while (cur->next != con->head && !con->check)
 	{
-		while (cur->next != con->head && !con->check)
-		{
-			if (pthread_create(&(cur->id), NULL, act_philo, (void *)cur))
-				con->error = "pthread_create error";
-			cur = cur->next;
-		}
-		cur = con->head;
-		while (cur->next != con->head && !con->check)
-		{
-			if (pthread_join(cur->id, NULL))
-				con->error = "pthread_join error";
-			cur = cur->next;
-		}
+		if (pthread_create(&(cur->id), NULL, act_philo, (void *)cur))
+			con->error = "pthread_create error";
+		cur = cur->next;
+	}
+	cur = con->head;
+	while (cur->next != con->head)
+	{
+		if (pthread_join(cur->id, NULL))
+			con->error = "pthread_join error";
+		cur = cur->next;
 	}
 	if (pthread_join(con->checker, NULL))
 		con->error = "pthread_join error";
