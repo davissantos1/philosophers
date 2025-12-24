0c0843821d563ccf91e580cdf16215924c36e0e4
davi santos <davisimoesdossantos1@gmail.com>
2025-12-23 21:47:08 -0300

    fix: invalid read solved

--- philo/memory.c
+++ philo/memory.c
@@ -6,7 +6,7 @@
 /*   By: dasimoes <dasimoes@42sp.org.br>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/17 16:46:02 by dasimoes          #+#    #+#             */
-/*   Updated: 2025/12/22 21:58:55 by dasimoes         ###   ########.fr       */
+/*   Updated: 2025/12/23 21:46:29 by dasimoes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,18 +17,22 @@ void	*free_all(t_control *control)
 	t_philo	*curr;
 	t_philo	*temp;
 
-	curr = control->head;
-	while (curr != control->head->prev)
+	curr = control->head->next;
+	temp = control->head->prev;
+	pthread_mutex_destroy(&control->head->fork);
+	pthread_mutex_destroy(&control->head->action_lock);
+	free(control->head);
+	while (curr)
 	{
-		temp = curr->next;
 		pthread_mutex_destroy(&curr->fork);
 		pthread_mutex_destroy(&curr->action_lock);
 		free(curr);
-		curr = temp;
+		if (curr == temp)
+			break ;
+		curr = curr->next;
 	}
 	pthread_mutex_destroy(&control->print_lock);
 	pthread_mutex_destroy(&control->check_lock);
-	free(curr);
 	free(control);
 	return (NULL);
 }
